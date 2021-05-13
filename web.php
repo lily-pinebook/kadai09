42eb6502efd518deb42930e0539daacb26cf74e3
松本悠里 <y_414snw@icloud.com>
2021-05-13 15:58:13 +0900

    kadai

--- web.php
+++ web.php
@@ -17,14 +17,18 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-      Route::post('news/create', 'Admin\NewsController@create'); #追記
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
-      Route::post('profile/create', 'Admin\ProfileController@create');
-      Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();
